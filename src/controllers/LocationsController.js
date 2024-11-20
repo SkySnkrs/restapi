4264984fec569bbc8e1945b9e66c7aefb67f0ad8
[SkySnkrs] <[skysnkrs@gmail.com]>
2024-11-20 15:08:13 -0700

    Added Get request for Missions at LocationId

--- src/controllers/LocationsController.js
+++ src/controllers/LocationsController.js
@@ -6,7 +6,19 @@ export class LocationsController extends BaseController {
         super('api/locations')
         this.router
             .get('', this.getLocations)
+            .get('/:locationId/missions', this.getMissionsByLocationId)
     }
+
+    async getMissionsByLocationId(request, response, next) {
+        try {
+            const locationId = request.params.locationId
+            const missions = await locationsServices.getMissionsByLocationId(locationId)
+            response.send(missions)
+        } catch (error) {
+            next(error)
+        }
+    }
+
     async getLocations(request, response, next) {
         try {
             const locations = await locationsServices.getLocations()
--- src/services/LocationsService.js
+++ src/services/LocationsService.js
@@ -1,6 +1,15 @@
 import { dbContext } from "../db/DbContext"
 
 class LocationsService {
+
+
+    async getMissionsByLocationId(locationId) {
+        const missions = await dbContext.Missions.find({ locationId: locationId }).populate('location').populate('rat', '-name -picture')
+
+        return missions
+    }
+
+
     getLocations() {
         const locations = dbContext.Locations.find()
         return locations
